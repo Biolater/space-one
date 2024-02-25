ea8336472090670637111c981d663dc5292a88a5
Biolater <yusifovmurad1@gmail.com>
2024-02-25 08:29:26 +0400

    Change layout of news section add loading state make it responsive

--- src/components/SpaceNews/SpaceNews.tsx
+++ src/components/SpaceNews/SpaceNews.tsx
@@ -13,43 +13,49 @@ type NewsItem = {
 
 const SpaceNews: FC = () => {
   const [news, setNews] = useState<Array<NewsItem>>([]);
-
+  const [loading, setLoading] = useState<Boolean>(true);
   useEffect(() => {
     const fetchData = async () => {
+      setLoading(true);
       try {
         const response = await axios.get(
           `https://gnews.io/api/v4/search?q=space+exploration&lang=en&max=5&apikey=${apiKey}`
         );
 
         const articles: NewsItem[] = response.data.articles;
-         
+
         setNews(articles);
       } catch (error) {
         console.error("Error fetching news:", error);
+      } finally {
+        setLoading(false);
       }
     };
-    console.log(news);
     fetchData();
-  }, [apiKey]); 
+  }, [apiKey]);
 
   return (
     <section id="news">
-      <div className="container px-4 mx-auto py-16">
+      <div className={`container px-4 mx-auto py-16 ${loading && "flex flex-col items-center"}`}>
         <h1 className="news__heading text-4xl mb-12 text-white font-semibold text-center">
           Learn more about space
         </h1>
-        <div className="news flex flex-col gap-6">
-          {/* Map through the news array and render NewsCard components */}
-          {news.map((item, idx) => (
-            <NewsCard
-              key={idx}
-              title={item.title}
-              description={item.description}
-              img={item.image}
-              link={item.url}
-            />
-          ))}
-        </div>
+        {loading && (
+          <div className="rays flex items-center after:w-40 after:h-40 justify-center"></div>
+        )}
+        {!loading && (
+          <div className="news flex flex-col gap-6">
+            {news.map((item, idx) => (
+              <NewsCard
+                key={idx}
+                title={item.title}
+                description={item.description}
+                img={item.image}
+                link={item.url}
+              />
+            ))}
+          </div>
+        )}
       </div>
     </section>
   );
