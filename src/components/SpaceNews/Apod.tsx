243e970e75b4cd987a8af5e459392daaaa99f295
Biolater <yusifovmurad1@gmail.com>
2024-02-26 11:45:59 +0400

    add astronomy picture of the day nasa api

---
+++ src/components/SpaceNews/Apod.tsx
@@ -0,0 +1,46 @@
+import { useState, useEffect } from "react";
+import ApodItem from "./ApodItem";
+import axios from "axios";
+type Apod = {
+  date: string;
+  explanation: string;
+  media_type: string;
+  service_version: string;
+  title: string;
+  url: string;
+};
+const nasaApiKey: string = import.meta.env.VITE_NASA_API_KEY;
+
+const Apod = () => {
+  const [nasaApod, setNasaApod] = useState<Apod>();
+  useEffect(() => {
+    const fetchData = async () => {
+      const response: Apod = (
+        await axios.get(
+          `https://api.nasa.gov/planetary/apod?api_key=${nasaApiKey}`
+        )
+      ).data;
+      setNasaApod(response);
+    };
+    fetchData();
+  }, []);
+  return (
+    <div className="apod">
+      <p className="apod__heading text-4xl mb-12 text-white font-semibold text-center">
+        Astronomy Picture Of The Day
+      </p>
+      <aside className="apod__content">
+        {nasaApod && (
+          <ApodItem
+            date={nasaApod?.date}
+            explanation={nasaApod?.explanation}
+            title={nasaApod?.title}
+            url={nasaApod?.url}
+          />
+        )}
+      </aside>
+    </div>
+  );
+};
+
+export default Apod;
--- src/components/SpaceNews/SpaceNews.tsx
+++ src/components/SpaceNews/SpaceNews.tsx
@@ -1,8 +1,8 @@
 import axios from "axios";
 import { FC, useEffect, useState } from "react";
 import NewsCard from "./NewsCard";
-
-const apiKey = import.meta.env.VITE_GNEWS_API_KEY as string;
+import Apod from "./Apod";
+const apiKey: string = import.meta.env.VITE_GNEWS_API_KEY as string;
 
 type NewsItem = {
   title: string;
@@ -19,11 +19,9 @@ const SpaceNews: FC = () => {
       setLoading(true);
       try {
         const response = await axios.get(
-          `https://gnews.io/api/v4/search?q=space+exploration&lang=en&max=5&apikey=${apiKey}`
+          `https://gnews.io/api/v4/search?q=space+exploration&lang=en&max=1&apikey=${apiKey}`
         );
-
         const articles: NewsItem[] = response.data.articles;
-
         setNews(articles);
       } catch (error) {
         console.error("Error fetching news:", error);
@@ -36,7 +34,11 @@ const SpaceNews: FC = () => {
 
   return (
     <section id="news">
-      <div className={`container px-4 mx-auto py-16 ${loading && "flex flex-col items-center"}`}>
+      <div
+        className={`container px-4 mx-auto py-16 ${
+          loading && "flex flex-col items-center"
+        }`}
+      >
         <h1 className="news__heading text-4xl mb-12 text-white font-semibold text-center">
           Learn more about space
         </h1>
@@ -44,17 +46,20 @@ const SpaceNews: FC = () => {
           <div className="rays flex items-center after:w-40 after:h-40 justify-center"></div>
         )}
         {!loading && (
-          <div className="news flex flex-col gap-6">
-            {news.map((item, idx) => (
-              <NewsCard
-                key={idx}
-                title={item.title}
-                description={item.description}
-                img={item.image}
-                link={item.url}
-              />
-            ))}
-          </div>
+          <>
+            <div className="news flex flex-col gap-6">
+              {news.map((item, idx) => (
+                <NewsCard
+                  key={idx}
+                  title={item.title}
+                  description={item.description}
+                  img={item.image}
+                  link={item.url}
+                />
+              ))}
+            </div>
+            <Apod />
+          </>
         )}
       </div>
     </section>
