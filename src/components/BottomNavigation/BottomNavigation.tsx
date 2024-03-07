50bdbb929035008bad273c21a0bd513bdcaf1c68
Biolater <yusifovmurad1@gmail.com>
2024-03-07 13:42:31 +0400

    Add log out to drawer and add new icons

--- src/components/BottomNavigation/BottomNavigation.tsx
+++ src/components/BottomNavigation/BottomNavigation.tsx
@@ -13,11 +13,14 @@ import MoreIcon from "@mui/icons-material/MoreVert";
 import Avatar from "@mui/material/Avatar";
 import Tooltip from "@mui/material/Tooltip";
 import Zoom from "@mui/material/Zoom";
+import LogoutIcon from "@mui/icons-material/Logout";
 import { List, ListItem, ListItemButton, ListItemIcon } from "@mui/material";
 import PeopleIcon from "@mui/icons-material/People";
 import { useState, useEffect } from "react";
-import { onAuthStateChanged } from "firebase/auth";
+import { onAuthStateChanged, signOut } from "firebase/auth";
 import { useNavigate } from "react-router";
+import Notifications from '@mui/icons-material/Notifications';
+import ExploreIcon from '@mui/icons-material/Explore';
 //@ts-ignore
 import { auth } from "../../firebase";
 import PersonSearch from "@mui/icons-material/PersonSearch";
@@ -38,6 +41,7 @@ type User = {
 export default function BottomAppBar() {
   const [isDrawerOpen, setIsDrawerOpen] = useState(false);
   const [user, setUser] = useState<User | null>(null);
+  const [isLoading, setIsLoading] = useState(true);
   const handleDrawerOpen = () => {
     setIsDrawerOpen(true);
   };
@@ -45,103 +49,206 @@ export default function BottomAppBar() {
     setIsDrawerOpen(false);
   };
   const navigate = useNavigate();
-
   useEffect(() => {
-    const unsubscribe = onAuthStateChanged(auth, (user) => {
-      if (user) {
-        setUser(user as User);
-      } else {
-        setUser(null);
-      }
-    });
-    return unsubscribe;
-  }, [auth]);
+    try {
+      const unsubscribe = onAuthStateChanged(auth, (user) => {
+        if (user) {
+          setUser(user as User);
+        } else {
+          setUser(null);
+        }
+        return unsubscribe;
+      });
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setIsLoading(false);
+    }
+  }, [auth, setUser, isLoading]);
 
+  const handleLogOut = () => {
+    signOut(auth).then(() => {
+      navigate("/login");
+      localStorage.removeItem("userLoggedin");
+    });
+  };
   return (
     <>
-      <CssBaseline />
-      <AppBar position="fixed" color="primary" sx={{ top: "auto", bottom: 0 }}>
-        <Toolbar>
-          <IconButton
-            color="inherit"
-            aria-label="open drawer"
-            onClick={() => handleDrawerOpen()}
+      {isLoading && <p>Loading...</p>}
+      {!isLoading && (
+        <>
+          <CssBaseline />
+          <AppBar
+            position="fixed"
+            color="primary"
+            sx={{ top: "auto", bottom: 0 }}
           >
-            <MenuIcon />
-          </IconButton>
-          <StyledFab color="secondary" aria-label="add">
-            <AddIcon />
-          </StyledFab>
-          <Box sx={{ flexGrow: 1 }} />
-          <IconButton color="inherit">
-            <SearchIcon />
-          </IconButton>
-          <IconButton color="inherit">
-            <MoreIcon />
-          </IconButton>
-        </Toolbar>
-      </AppBar>
-      <Drawer
-        sx={{ backgroundColor: "#191919" }}
-        open={isDrawerOpen}
-        onClose={() => handleDrawerClose()}
-        PaperProps={{
-          className: "customDrawerPaper",
-        }}
-      >
-        <List>
-          <Tooltip
-            TransitionComponent={Zoom}
-            title="Add friends"
-            placement="right"
+            <Toolbar>
+              <IconButton
+                color="inherit"
+                aria-label="open drawer"
+                onClick={() => handleDrawerOpen()}
+              >
+                <MenuIcon />
+              </IconButton>
+              <StyledFab color="secondary" aria-label="add">
+                <AddIcon />
+              </StyledFab>
+              <Box sx={{ flexGrow: 1 }} />
+              <IconButton color="inherit">
+                <SearchIcon />
+              </IconButton>
+              <IconButton color="inherit">
+                <MoreIcon />
+              </IconButton>
+            </Toolbar>
+          </AppBar>
+          <Drawer
+            sx={{ backgroundColor: "#191919", "& .MuiDrawer-paper" : {justifyContent: "space-between"}, }}
+            open={isDrawerOpen}
+            onClose={() => handleDrawerClose()}
+            PaperProps={{
+              className: "customDrawerPaper",
+            }}
           >
-            <ListItem disablePadding>
-              <ListItemButton
-                onClick={() => navigate("/users")}
-                sx={{
-                  color: "white",
-                  justifyContent: "center",
-                  "&:hover": { backgroundColor: "#333" },
-                }}
+            <List>
+            <Tooltip
+                TransitionComponent={Zoom}
+                title="Explore"
+                placement="right"
               >
-                <ListItemIcon sx={{ minWidth: "unset", display: "block" }}>
-                  <PersonSearch sx={{ color: "#fff" }} />
-                </ListItemIcon>
-              </ListItemButton>
-            </ListItem>
-          </Tooltip>
-          <Tooltip TransitionComponent={Zoom} title="Friends" placement="right">
-            <ListItem disablePadding>
-              <ListItemButton
-                onClick={() => navigate("/friends")}
-                sx={{
-                  color: "white",
-                  justifyContent: "center",
-                  "&:hover": { backgroundColor: "#333" },
-                }}
+                <ListItem disablePadding>
+                  <ListItemButton
+                    onClick={() => navigate("/explore")}
+                    sx={{
+                      width: "60px",
+                      height: "60px",
+                      borderRadius: "50%",
+                      color: "white",
+                      justifyContent: "center",
+                      "&:hover": { backgroundColor: "#333" },
+                    }}
+                  >
+                    <ListItemIcon sx={{ minWidth: "unset", display: "block" }}>
+                      <ExploreIcon sx={{ color: "#fff" }} />
+                    </ListItemIcon>
+                  </ListItemButton>
+                </ListItem>
+              </Tooltip>
+              <Tooltip
+                TransitionComponent={Zoom}
+                title="Notifications"
+                placement="right"
               >
-                <ListItemIcon sx={{ minWidth: "unset", display: "block" }}>
-                  <PeopleIcon sx={{ color: "#fff" }} />
-                </ListItemIcon>
-              </ListItemButton>
-            </ListItem>
-          </Tooltip>
-        </List>
-        <Tooltip
-          TransitionComponent={Zoom}
-          title="Go to profile"
-          placement="right"
-        >
-          <Avatar
-            onClick={() => navigate("/dashboard")}
-            src={
-              user?.photoURL ||
-              "https://cdn.pixabay.com/photo/2012/04/26/19/43/profile-42914_1280.png"
-            }
-            sx={{ width: 60, height: 60, cursor: "pointer" }}
-          />
-        </Tooltip>
-      </Drawer>
+                <ListItem disablePadding>
+                  <ListItemButton
+                    onClick={() => navigate("/notifications")}
+                    sx={{
+                      width: "60px",
+                      height: "60px",
+                      borderRadius: "50%",
+                      color: "white",
+                      justifyContent: "center",
+                      "&:hover": { backgroundColor: "#333" },
+                    }}
+                  >
+                    <ListItemIcon sx={{ minWidth: "unset", display: "block" }}>
+                      <Notifications sx={{ color: "#fff" }} />
+                    </ListItemIcon>
+                  </ListItemButton>
+                </ListItem>
+              </Tooltip>
+              <Tooltip
+                TransitionComponent={Zoom}
+                title="Add friends"
+                placement="right"
+              >
+                <ListItem disablePadding>
+                  <ListItemButton
+                    onClick={() => navigate("/users")}
+                    sx={{
+                      width: "60px",
+                      height: "60px",
+                      borderRadius: "50%",
+                      color: "white",
+                      justifyContent: "center",
+                      "&:hover": { backgroundColor: "#333" },
+                    }}
+                  >
+                    <ListItemIcon sx={{ minWidth: "unset", display: "block" }}>
+                      <PersonSearch sx={{ color: "#fff" }} />
+                    </ListItemIcon>
+                  </ListItemButton>
+                </ListItem>
+              </Tooltip>
+              <Tooltip
+                TransitionComponent={Zoom}
+                title="Friends"
+                placement="right"
+              >
+                <ListItem disablePadding>
+                  <ListItemButton
+                    onClick={() => navigate("/friends")}
+                    sx={{
+                      width: "60px",
+                      height: "60px",
+                      borderRadius: "50%",
+                      color: "white",
+                      justifyContent: "center",
+                      "&:hover": { backgroundColor: "#333" },
+                    }}
+                  >
+                    <ListItemIcon sx={{ minWidth: "unset", display: "block" }}>
+                      <PeopleIcon sx={{ color: "#fff" }} />
+                    </ListItemIcon>
+                  </ListItemButton>
+                </ListItem>
+              </Tooltip>
+            </List>
+            <List
+              sx={{ display: "flex", flexDirection: "column", gap: "10px" }}
+            >
+              <Tooltip
+                TransitionComponent={Zoom}
+                title="Log out"
+                placement="right"
+              >
+                <ListItem disablePadding>
+                  <ListItemButton
+                    onClick={() => handleLogOut()}
+                    sx={{
+                      width: "60px",
+                      height: "60px",
+                      borderRadius: "50%",
+                      color: "white",
+                      justifyContent: "center",
+                      "&:hover": { backgroundColor: "#333" },
+                    }}
+                  >
+                    <ListItemIcon sx={{ minWidth: "unset", display: "block" }}>
+                      <LogoutIcon sx={{ color: "#fff" }} />
+                    </ListItemIcon>
+                  </ListItemButton>
+                </ListItem>
+              </Tooltip>
+              <Tooltip
+                TransitionComponent={Zoom}
+                title="Go to profile"
+                placement="right"
+              >
+                <Avatar
+                  onClick={() => navigate("/dashboard")}
+                  src={
+                    user?.photoURL ||
+                    "https://cdn.pixabay.com/photo/2012/04/26/19/43/profile-42914_1280.png"
+                  }
+                  sx={{ width: 60, height: 60, cursor: "pointer" }}
+                />
+              </Tooltip>
+            </List>
+          </Drawer>
+        </>
+      )}
     </>
   );
 }
