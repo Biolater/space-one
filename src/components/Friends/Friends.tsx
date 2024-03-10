5d05351a11cf0d54f051cd9be0c68dc8135ba465
Murad <yusifovmurad1@gmail.com>
2024-03-10 09:23:59 +0400

    Added friends section

---
+++ src/components/Friends/Friends.tsx
@@ -0,0 +1,9 @@
+import { Typography } from "@mui/material";
+
+export default function Friends() {
+  return (
+    <div className="h-screen flex justify-center items-center">
+        <Typography variant="h2" fontWeight={500}>Friends</Typography>
+    </div>
+  )
+}
--- src/main.tsx
+++ src/main.tsx
@@ -1,20 +1,32 @@
-import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
-import "./index.css";
-import Login from "./components/Navbar/Login.jsx";
-import Signup from "./components/Navbar/Signup.tsx";
-import Dashboard from "./components/Dashboard/Dashboard.tsx";
-ReactDOM.createRoot(document.getElementById("root")!).render(
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import App from './App.tsx';
+import './index.css';
+import Login from './components/Navbar/Login.jsx';
+import Signup from './components/Navbar/Signup.tsx';
+import Dashboard from './components/Dashboard/Dashboard.tsx';
+import Friends from './components/Friends/Friends.tsx';
+
+const theme = createTheme({
+  typography: {
+    fontFamily: 'Montserrat, sans-serif',
+  },
+});
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <Router>
-      <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/dashboard" element={<Dashboard />}></Route>
-      </Routes>
-    </Router>
+    <ThemeProvider theme={theme}>
+      <Router>
+        <Routes>
+          <Route path="/" element={<App />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/friends" element={<Friends />} />
+        </Routes>
+      </Router>
+    </ThemeProvider>
   </React.StrictMode>
 );
