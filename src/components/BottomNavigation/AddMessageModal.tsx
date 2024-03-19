21eb7ac4f0f26c0828c9a96d136388e4219305a4
Murad <yusifovmurad1@gmail.com>
2024-03-19 13:53:27 +0400

    Added message adding feature to db

--- src/components/BottomNavigation/AddMessageModal.tsx
+++ src/components/BottomNavigation/AddMessageModal.tsx
@@ -1,21 +1,80 @@
-import { RefObject } from "react";
+import { RefObject, useEffect, useState } from "react";
+import { User } from "firebase/auth";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+import { getFirestore, collection, addDoc, serverTimestamp } from "firebase/firestore"; 
 
 type MessageModalType = {
   isModalOpen: boolean;
   modalRef: RefObject<HTMLDivElement>;
 };
 
-
-
 const AddMessageModal = ({ isModalOpen, modalRef }: MessageModalType) => {
-  
+  const [message, setMessage] = useState<string>("");
+  const [user, setUser] = useState<User | null>(null);
+  const db = getFirestore(); // Initialize Firestore
+  const handleMessage = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setMessage(event.target.value);
+  }
+  useEffect(() => {
+    const auth = getAuth();
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      if (user) {
+        setUser(user);
+      } else {
+        setUser(null);
+      }
+    });
+    return unsubscribe; 
+  }, []);
+  const handleMessageAdd = async () => {
+    if (message.length === 0 || !user) return; // Validation
+    try {
+      const messageCollectionRef = collection(db, "messages");
+      await addDoc(messageCollectionRef, {
+        message: message,
+        username: user.displayName,
+        uid: user?.uid,
+        userAvatar: user?.photoURL,
+        createdAt: serverTimestamp(),
+      });
+      setMessage('');
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
   return (
-    <div ref={modalRef}  className={`messageModal transition-all duration-300 fixed top-0 modal-overlay w-full h-screen bg-[rgba(0,0,0,0.5)] flex items-center justify-center ${isModalOpen ? "opacity-1 pointer-events-auto" : "opacity-0 pointer-events-none"}  `}>
+    <div
+      ref={modalRef}
+      className={`messageModal transition-all duration-300 fixed top-0 modal-overlay w-full h-screen bg-[rgba(0,0,0,0.5)] flex items-center justify-center ${
+        isModalOpen
+          ? "opacity-1 pointer-events-auto"
+          : "opacity-0 pointer-events-none"
+      }  `}
+    >
       <div className="container px-4 mx-auto">
-        <div className="messageModal__container min-w-full min-h-[200px] bg-white rounded-2xl"></div>
+        <div className="messageModal__container flex flex-col justify-center items-center min-w-full  p-3 bg-[#fff] rounded-2xl">
+          <p className="mb-2 font-semibold text-xl">What is on your mind ?</p>
+          <input
+            type="text"
+            value={message}
+            onChange={(event) => handleMessage(event)}
+            className="block w-full px-4 py-2 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 text-base focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            id="large-input"
+          />
+          {message.length > 0 && (
+            <button
+              onClick={handleMessageAdd}
+              type="button"
+              className="py-3 mt-2 px-4 inline-flex items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent bg-gray-800 text-white hover:bg-gray-900 disabled:opacity-50 disabled:pointer-events-none "
+            >
+              Send
+            </button>
+          )}
+        </div>
       </div>
     </div>
   );
-};  
+};
 
 export default AddMessageModal;
--- src/components/Messages/Messages.tsx
+++ src/components/Messages/Messages.tsx
@@ -1,5 +1,5 @@
 import MessageCard from "../MessageCard/MessageCard";
-import { faker } from "@faker-js/faker";
+import { getFirestore, collection, getDocs, query, orderBy } from "firebase/firestore"; 
 type Messages = {
   id: number;
   username: string;
@@ -7,35 +7,15 @@ type Messages = {
   message: string;
   time: Date;
 };
+
+
 const Messages = () => {
-  const generateMessages = (numMessages: number) => {
-    const messages: Messages[] = [];
-    for (let i = 0; i < numMessages; i++) {
-      messages.push({
-        id: i,
-        username: faker.internet.userName(),
-        avatar: faker.image.avatar(),
-        message: faker.lorem.sentence(),
-        time: faker.date.recent(),
-      });
-    }
-    return messages;
-  };
-  console.log(generateMessages(5)); 
   return (
     <div className="messages md:ps-[90px]">
       <div className="container mx-auto px-4 py-6">
         <h1 className="text-2xl text-white font-semibold mb-4">Messages</h1>
         <div className="grid gap-6">
-          {generateMessages(25).map((message: Messages) => (
-            <MessageCard
-              key={message.id}  
-              message={message.message}
-              avatar={message.avatar}
-              time={message.time}
-              username={message.username}
-            />
-          ))}
+
         </div>
       </div>
     </div>
